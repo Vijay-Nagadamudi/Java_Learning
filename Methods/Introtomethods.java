d803136b9cc8273442d2eb63bc95385f349a12d0
Vijay Nagadamudi <vijaynagadamudi525@gmail.com>
2026-02-13 23:10:59 +0530

    Add files via upload

---
+++ Methods/Introtomethods.java
@@ -0,0 +1,19 @@
+package METHODS;
+
+public class Introtomethods {
+    public static void main(String[] args) {
+        System.out.println("Hello Guys....");
+        Greet();
+        System.out.println("This is Vijay");
+        Intro();
+
+    }
+     public static void Intro(){
+         System.out.println("This is My project");
+     }
+
+    public static void Greet() {
+        System.out.println("Good morning");
+
+    }
+}
---
+++ Methods/maxofTHREE.java
@@ -0,0 +1,7 @@
+package METHODS;
+
+public class maxofTHREE {
+    public static void main(String[] args) {
+        System.out.println(Math.max(4,Math.max(5,2)));
+    }
+}
